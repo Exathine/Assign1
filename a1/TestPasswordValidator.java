c43c40cd9a8c36835c9da5cd907d9a965696f792
Your Name <you@example.com>
2015-01-30 14:55:10 -0500

    Pre-Submission
    Final Product

--- a1/TestPasswordValidator.java
+++ a1/TestPasswordValidator.java
@@ -7,20 +7,20 @@ public class TestPasswordValidator {
 	@Test
 	public void testPasswordLength()
 	{
-		int length = PasswordValidator.getPasswordLength("Password01");
+		int length = PasswordValidator.getPasswordLength("Pass01");
 		assertTrue("The password supplied is not long enough", length > 8);
 	}
 	
 	@Test
 	public void testPasswordCharacters()
 	{
-		boolean pass = PasswordValidator.checkValidPassword("Password@@");
+		boolean pass = PasswordValidator.checkValidPassword("Password02");
 		assertTrue("Password contains invalid characters.", pass==true);
 	}
 	@Test
 	public void testDigits()
 	{
-		int n = PasswordValidator.checkDigits("Password03");
+		int n = PasswordValidator.checkDigits("Passwords3");
 		assertTrue("Password requires at least 2 numbers.", n > 1);
 	}
 }
